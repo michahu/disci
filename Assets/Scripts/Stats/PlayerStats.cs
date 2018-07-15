e813556aceb0a88238b4cb8a20f9d7bbf9ed9a3c
Jason Tan <jasonzhetan@yahoo.com>
2018-07-15 18:56:45 -0400

    Added Damage Modifiers, FIguring Out Turn-Based Attack

--- Assets/Scripts/Stats/PlayerStats.cs
+++ Assets/Scripts/Stats/PlayerStats.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStats : MonoBehaviour {
+public class PlayerStats : CharacterStats {
 
 	// Use this for initialization
 	void Start () {
-		
+        EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
 	}
+
+    void OnEquipmentChanged (Equipment newItem, Equipment oldItem)
+    {
+        if (newItem != null)
+        {
+            armor.AddModifier(newItem.armorModifier);
+            damage.AddModifier(newItem.damageModifier);
+        }
+
+        if (oldItem != null)
+        {
+            armor.RemoveModifier(oldItem.armorModifier);
+            damage.RemoveModifier(oldItem.damageModifier);
+        }
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
