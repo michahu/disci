3e5b9123a104d687fa0af1bbca9a2fcb0f8b6343
Jason Tan <jasonzhetan@yahoo.com>
2018-07-18 14:29:36 -0400

    Loaded Health Bar images, created new HelathUI script (to be integrated with attacking)

---
+++ Assets/Scripts/HealthUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour {
+
+    public GameObject uiPrefab;
+    public Transform target;
+
+    Transform ui;
+    Image healthSlider;
+
+	// Use this for initialization
+	void Start () {
+		foreach (Canvas c in FindObjectsOfType<Canvas>())
+        {
+            ui = Instantiate(uiPrefab, c.transform).transform;
+            ui.position = target.position;
+            healthSlider = ui.GetChild(0).GetComponent<Image>();
+            break;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
