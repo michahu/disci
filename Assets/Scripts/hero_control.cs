b628133cfb80a78aa097fbcb6dda3a9b464b8be4
Hunter Sieben <hsieben@princeton.edu>
2018-07-20 00:15:07 +0100

    player attack and video

---
+++ Assets/Scripts/hero_control.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hero_control : MonoBehaviour {
+
+
+    void Start() {
+
+    }
+
+
+    void Update () {
+
+        if (Input.GetKeyDown ("space")) 
+        {
+
+            GetComponent<Animator> ().SetTrigger ("hero_melee");
+
+            GetComponent<Transform> ().position = new Vector2 (-275.65f, 73.29f);
+
+        }
+    }
+
+    void returnHero ()
+    {
+
+        GetComponent<Transform>().position = new Vector2 (-180f, -91f);
+
+    }
+}
+
+
