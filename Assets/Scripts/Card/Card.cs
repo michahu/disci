9ae8beb94e402e1ed15f1fd9ed558b229884f309
mikkyhu <myhu@princeton.edu>
2018-07-20 18:08:00 -0400

    Abstract card class

---
+++ Assets/Scripts/Card/Card.cs
@@ -0,0 +1,12 @@
+public abstract class Card
+{
+    abstract void Attack();
+
+    abstract void Defend();
+
+    abstract void Heal();
+
+    abstract void Buff();
+
+    abstract void Debuff();
+}
