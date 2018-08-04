d6dd459bb6b69dfd317ebfe1fa20a9985c9b7562
Jason Tan <jasonzhetan@yahoo.com>
2018-08-04 17:02:05 -0400

    Combined Code for BuyCard w/ Shop & CardDisplay w/ CardUI

--- Assets/Scripts/Card/CardUI.cs
+++ Assets/Scripts/Card/CardUI.cs
@@ -5,14 +5,54 @@ using UnityEngine.UI;
 
 public class CardUI : MonoBehaviour {
 
+    public GameObject gameManager;
     public GameObject CardPrefab;
 
     Transform ui;
+    CardData ShopCardData;
+    Card[] AllCards;
+
+    [HideInInspector]
+    public Card card;
+
     Image artwork;
     Text name;
     Text description;
     Text cost;
 
+    // for shop cards
+    public void LoadCard(int i)
+    {
+        // setting the sample card and checking for null
+        ShopCardData = gameManager.GetComponent<DataController>().allCards[0];
+        AllCards = ShopCardData.cards;
+
+        if (i < AllCards.Length)
+        {
+
+            card = AllCards[i];
+
+            // instantiate Card prefab
+            ui = Instantiate(CardPrefab).transform;
+            ui.transform.SetParent(this.transform, false);
+            ui.position = this.transform.position;
+
+            // defining all components in instantiated prefab
+            artwork = ui.GetChild(0).GetChild(0).GetComponent<Image>();
+            name = ui.GetChild(2).GetComponent<Text>();
+            description = ui.GetChild(3).GetComponent<Text>();
+            cost = ui.GetChild(4).GetComponent<Text>();
+
+            // uploading with desired components
+            artwork.sprite = card.artwork;
+            name.text = card.name;
+            description.text = card.description;
+            cost.text = card.cost.ToString();
+        }
+
+    }
+
+    // for hand cards
     public void AddCard(Card newCard)
     {
         ui = Instantiate(CardPrefab).transform;
--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.EventSystems;
 
 public class ShopManager : MonoBehaviour {
 
@@ -14,6 +15,7 @@ public class ShopManager : MonoBehaviour {
     public GameObject AdvButton; // shitty fix, wil remove
 
     GameObject[] cards;
+    Card card;
 
     public void StartShop()
     {
@@ -22,7 +24,7 @@ public class ShopManager : MonoBehaviour {
         CannotAfford.SetActive(false);
         // loading each card
 
-        cards = GameObject.FindGameObjectsWithTag("Card").OrderBy(go => go.name).ToArray();;
+        cards = GameObject.FindGameObjectsWithTag("ShopCard").OrderBy(go => go.name).ToArray();;
 
         for (int j = 0; j < cards.Length; j++)
         {
@@ -32,9 +34,26 @@ public class ShopManager : MonoBehaviour {
         int i = 0;
         foreach (GameObject card in cards)
         {
-            card.GetComponent<CardDisplay>().LoadCard(i);
+            card.GetComponent<CardUI>().LoadCard(i);
             i++;
         }
+    }
+
+    public void Buy(GameObject target)
+    {
+        card = target.GetComponent<CardUI>().card;
+
+        if (IsThereMoney(card.cost))
+        {
+            Debug.Log("Buying card " + card.name);
+
+            Hand.handInstance.Add(card);
+
+            Money.SubtractMoney(card.cost);
+        }
+
+        else return;
+
     }
 
     public bool IsThereMoney(int cost)
