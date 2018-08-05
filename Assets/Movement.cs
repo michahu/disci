d2423680a00019320ce4d5eeb4a532d2d5ccec63
Hunter Sieben <hsieben@princeton.edu>
2018-08-05 09:50:22 -0500

    Movement and Scene Trigger

---
+++ Assets/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	void Start () {
+
+
+		
+	}
+	
+	void Update () {
+
+
+        transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime, 0f, Input.GetAxis("Vertical")*Time.deltaTime);
+
+		
+	}
+}
---
+++ Assets/SceneTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTrigger : MonoBehaviour {
+
+    public GameObject guiObject;
+    public string Level1;
+
+	void Start () {
+        guiObject.SetActive(false);
+        
+		
+	}
+	
+    void OnTriggerStay (Collider other) {
+
+        if (other.gameObject.tag == "Player") {
+
+
+            guiObject.SetActive(true);
+            if (guiObject.activeInHierarchy == true && Input.GetButtonDown("Use"))
+            {
+
+                Application.LoadLevel(Level1);
+            }
+        }
+		
+	}
+
+	void OnTriggerExit()
+	{
+        guiObject.SetActive(false);
+	}
+}
