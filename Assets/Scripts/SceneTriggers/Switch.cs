e82eaececb44172abdfcf8233b63c341cf88c7b1
Hunter Sieben <hsieben@princeton.edu>
2018-08-09 16:41:18 -0400

    Fixed Bugs for Trigger Change

---
+++ Assets/Scripts/SceneTriggers/Switch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Switch : MonoBehaviour {
+
+    public string levelToLoad;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other) {
+
+        if(other.tag == "Player") {
+
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
