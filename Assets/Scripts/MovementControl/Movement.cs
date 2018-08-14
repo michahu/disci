1d9375baf48666a89e1bf689a505c18b67faaca1
Hunter Sieben <hsieben@princeton.edu>
2018-08-14 02:31:40 -0400

    New Battle Scene & Music

---
+++ Assets/Scripts/MovementControl/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float moveSpeed;
+
+	void Start () {
+
+        moveSpeed = 2f;
+		
+	}
+	
+	void Update () {
+
+
+        transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime, 0f, moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
+
+		
+	}
+}
---
+++ Assets/Scripts/Video/panelopen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+
+public class panelopen : MonoBehaviour
+{
+
+    public GameObject Panel;
+    int counter;
+
+    public void showhidepanel()
+    {
+        counter++;
+        if (counter % 2 == 1)
+        {
+            Panel.gameObject.SetActive (false);
+
+        } else { 
+        
+            Panel.gameObject.SetActive (true);
+        
+        }
+    }
+}
+
+
