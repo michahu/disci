7f9e929c200bc0acaeef3763e373926be020ae52
mikkyhu <myhu@princeton.edu>
2018-09-22 16:24:52 -0400

    Deck is now a toggle

---
+++ Assets/Scripts/ShowDeck.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowDeck : MonoBehaviour {
+
+    public GameObject deck;
+    private bool isVisible = false;
+
+	// Use this for initialization
+	void Start () {
+        deck.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	public void ToggleDeck () {
+        deck.SetActive(!isVisible);
+        isVisible = !isVisible;
+	}
+}
