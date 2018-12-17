0c99191c083dc178b017a7b3030dd0944464371d
Hunter Sieben <hsieben@princeton.edu>
2018-12-16 19:30:28 -0500

    New Enemy Import

---
+++ Assets/Scripts/Question Vid.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionVid : MonoBehaviour
+
+{
+    "array": [
+        {
+            "q": "What are the three main types of atoms and their respective charges?",
+            "tags": "Atoms",
+            "ans": 1,
+            "answers": [
+                "Proton, electron, neutron",
+                "Positron, negatron, megatron",
+                "Proton, electron, neutron",
+                "Neutron, neuron, synapse"
+            ]
+            }, 
+
+
+        question: "Which of the following is the strongest intermolecular force?",
+
+        a: Hydrogen bonds
+        b: Van der Waals
+        c: Dipole - dipole
+        d: London Dispersion
+
+        question: "What determines the relative strength of a dipole - dipole moment?",
+
+        a: Difference in electronegativity
+
+        question: "What is an atom’s electronegativity?",
+
+        a: A measure of the tendency of an atom to attract a bonding pair of electrons
+
+        question: "Which of the following is the strongest Van der Waals Force?",
+
+        a: Hydrogen Bonds
+        b: London Dispersion
+        c: Dipole - Dipole
+        d: Ionic Compounds
+
+        question: "What is the distinguishing property of London Dispersion Forces?",
+
+        a: Spontaneous electron configuration produces a slightly negative charge
+
+        question: "Which of the following situations is most likely to attract a hydrogen bond?",
+        a: Another hydrogen molecule
+        b: An electronegative oxygen
+        c: A Helium molecule
+
+        question: "Which of the following subatomic particles has the largest atomic mass?",
+
+        a: Neutron
+        b: Proton
+        c: Electron
+        d: Quark
+
+        question: "Where are protons located within an atom?",
+
+        a: Within the Nucleus with neutrons
+        b: Within neutrons
+        c: In the nucleus with electrons
+        d: Outside of the nucleus
+
+        question: "What types of molecules are involved in dipole - dipole attractions?",
+        a: polar molecules
+        b: nonpolar molecules
+        c: noble gases
+        d: carbon ions
+
+        question: "What are the seven basic units of the SI system?",
+        a: Kg, A, mol, K, Cd, m, s
+
+        question: "Why is the SI system used as a measurement standard?",
+        a: For Fluidity cohesiveness across scientific studies, ease of replicating experiments
+
+        question: "Which of the following is the most appropriate units of measurement if we were measuring the amount " +
+        "of mass lost due to radioactive decay of a particular substance over a short period of time?",
+        a: "Grams and seconds / minutes"
+
+        question: "How would you express 584, 000, 000, 000 in scientific notation?",
+
+        quesiton: "How would you express .00004823 in scientific notation?",
+
+        question: "How would you express 8.32 x 10 ^ 6 numerically?",
+        question: "(4.1357 x 10 ^ -14)(5.4 x 10 ^ 2) = ?",
+
+        question: "Which unit is most appropriate for expressing luminous intensity?",
+
+        question: "What is the relationship between a chemical substance’s measured grams and moles?",
+
+        question: "Why do we use Kelvin as the standard unit measurement of time rather than Fahrenheit or Celsius?",
+        a: Gas law problems are dependent on kinetic energy and the Kelvin scale is proportional to the KE of particles, ie. 0K = 0KE
+
+        question: "Na3PO4 + CaCl2  NaCl + Ca3(PO4)2",
+        question: "NaOH + H2SO4  Na2SO4 + H2O",
+        question: "Al + HCl  AlCl3 + H2",
+        question: "A Chemist is working with a sample of methanol with 6.7 g of carbon, " +
+        "3.6 g of hydrogen, and 16.3 g of oxygen.What is the percent composition of each element in the methanol sample?",
+        question : "What is the percent mass composition for each element in glucose, C6H12O6?"
+        question : "CH4 + O2  CO2 + H20.Balance the following equation and then " +
+        "calculate the mass(g) of hydrogen in the reactants and products.",
+        question: "CH4 + 2O2  CO2 + 2H2O",
+        a: Calculate the amount of moles used in the reactants of the above substance.
+        b: Calculate the expected moles of the of the product and then the respective expected molar mass.
+        question: "Given a 100 g sample of N - 16 that decays to 13 g of N - 16 in 22s, " +
+        "what is the half - life of N - 16 ? Ln(N / N0) = (-0.693 / t1 / 2)t?",
+        question: "Carbon burns in oxygen to make carbon dioxide. 15 g of carbon burned " +
+        "and produced 35 g of CO2.What was the percentage yield in the C + O2  CO2 equation?",
+        question: "Balance this equation.H2CO3  H2O + CO2",
+
+        question: "What are the two categories of observable compounds?",
+        question: "What type of compound / mixture is distinguished by carbon backbones and covalent bonds?",
+        question: "What are the inorganic, carbon - containing compounds?",
+        question: "What causes the hydrophobic nature of organic compounds?",
+        question: "What are the four essential subcategories of inorganic compounds?",
+        question: "What is the main difference between compounds and mixtures?",
+        question: "Which of the below categories appropriately describes a packet of Country Time lemonade dissolved in water?",
+        question: "What is the difference between a homogeneous mixture and an inorganic compound?",
+        question: "How would we classify the following substance: Na2SO2?",
+        question: "How would we classify the following substance: C2H4O2?",
+
+        question: "What do the columns on a periodic table tell us about an element’s electronegativity?",
+        question: "What is the net ionic charge of oxygenates?",
+        question: "What is the net ionic charge of noble gases?",
+        question: "What does the name of an ionic compound tell you about the electronegativity of involved substances?",
+        question: "What are the seven naturally occurring diatomic molecules?",
+        question: "What distinguishes diatomic molecules from ionic compounds?",
+        question: "Name the following ionic compound.KCl.",
+        question: "Name the following ionic compound.NH4OH.",
+        question: "Name the following ionic compound and calculate the molar mass of the negatively charged compound: NaOH.",
+        question: "Balance the following chemical equation, both charges and molecules.MnO4 - +H +  Mn2 + +4H2O.",
+
+        question: "How many significant figures are in each of the following measurements?",
+        a:  7.030 x 10 ^ -7
+        b:  504.1
+        c:  0.0409
+
+        question: "Complete the following calculation with the appropriate number of significant figures.",
+        a:  7.433 + 8.6 – 2.114
+        b:  17.32 + .003
+        c:  542 * 0.032
+        d:  .032 / 12.4
+
+        question: "Using appropriate significant figures.How many moles are in 63 grams of Cu(OH)2?",
+        question: "Using appropriate significant figures.How many grams are in 3.6 mol of NaF?",
+        question: "Using appropriate significant figures.How many molecules are in 320 grams of methane?",
+        question: "When are trailing zeroes included in a significant figure count?,"
+        a: In decimals
+
+        question: "Round 6843.902 to 2 sf.",
+        question: "Which of the following is not a rule for significant figures?",
+        a: All nonzero digits are significant
+        b: Zeros between other nonzeros are significant
+        c: Zeros in front of nonzeros are not significant
+        d: Zeros at the end of a number and to the right of the decimal are significant
+        e: Zeros at the end of the number but to the left of the decimal are signigifacnt if they have been measures or are the first estimated digit.
+        question: "Round 31.463 to the nearest tenth.How many significant figures does the new estimate have?",
+        question: "Your measuring device estimates your post - Thanksgiving weight at 187.4 pounds. " +
+        "Which of the following numbers are you at the liberty of claiming considering significant figure guidelines?",
+
+
+    ];
+
+}
+
+
+
+
+
+
+
+
+
